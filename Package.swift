f6bd2bdca9be5b5ade98edb1ebe0dc8cad63bf70
Eugene Dymov <edymov@atlassian.com>
2021-02-03 17:39:02 +1100

    Add iOS platform to manifest

--- Package.swift
+++ Package.swift
@@ -1,9 +1,12 @@
-// swift-tools-version:4.2
+// swift-tools-version:5.3
 
 import PackageDescription
 
 let package = Package(
     name: "Operadics",
+    platforms: [
+        .iOS(.v10)
+    ],
     products: [
         .library(name: "Operadics", targets: ["Operadics"]),
     ],
